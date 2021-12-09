7740b0fede91f5b100294f24496890167d88f5fe
Declan Naughton <declan.naughton2@mail.dcu.ie>
2021-12-09 13:01:25 +0000

    some annuity values

---
+++ src/annuity.cul.js
@@ -0,0 +1,16 @@
+// I => how much annuity? => fn P(I) => P
+// P => how much I? => fn I(P) => I
+
+// fixed interest rate, this can be overridden
+export const i = () => i_in;
+export const term = () => term_in;
+export const P$ = () => P$_in;
+export const I$ = () => I$_in;
+
+// I = P*a, i
+
+export const P = () => (P$() ? P$() : I() / a()); // P(P$_in, I$_in)
+export const I = () => (I$() ? I$() : P() * a());
+
+export const a = () => (1 - Math.pow(v(), term())) / i(); // this should be a PV to work for variable i esp.
+export const v = () => 1 / (1 + i());
---
+++ src/use-annuity.js
@@ -0,0 +1,6 @@
+var m = require('./annuity');
+
+console.log(m.v({ i_in: 0.04, term_in: 10 }));
+console.log(m.a({ i_in: 0.04, term_in: 10 }));
+console.log(m.P({ I$_in: 100000, i_in: 0.04, term_in: 10 })); // validated Pg 56 "Compound Interest" 4%  https://www.actuaries.org.uk/system/files/field/document/Formulae%20and%20Tables.pdf
+console.log(m.I({ P$_in: 10000, i_in: 0.04, term_in: 10 }));
