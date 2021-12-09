098f83e880ecedd0fff936f57d3208af0cc349d8
Declan Naughton <declan.naughton2@mail.dcu.ie>
2021-12-09 13:26:40 +0000

    script updates

--- src/use-annuity.js
+++ src/use-annuity.js
@@ -2,5 +2,8 @@ var m = require('./annuity');
 
 console.log(m.v({ i_in: 0.04, term_in: 10 }));
 console.log(m.a({ i_in: 0.04, term_in: 10 }));
-console.log(m.P({ I$_in: 100000, i_in: 0.04, term_in: 10 })); // validated Pg 56 "Compound Interest" 4%  https://www.actuaries.org.uk/system/files/field/document/Formulae%20and%20Tables.pdf
+console.log(m.a({ i_in: 0.04, term_in: 10 }) - 8.1109); // chk vs Pg 56 "Compound Interest" 4%  https://www.actuaries.org.uk/system/files/field/document/Formulae%20and%20Tables.pdf
+console.log(m.P({ I$_in: 100000, i_in: 0.04, term_in: 10 }));
 console.log(m.I({ P$_in: 10000, i_in: 0.04, term_in: 10 }));
+
+// typings
